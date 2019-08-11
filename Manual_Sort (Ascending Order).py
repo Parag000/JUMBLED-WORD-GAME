2159879f762fb7abe3d610eb5b073affc36cd7b1
PARAG DESHPANDE <53427191+Parag000@users.noreply.github.com>
2019-08-11 12:38:27 +0530

    Add files via upload

---
+++ Manual_Sort (Ascending Order).py
@@ -0,0 +1,23 @@
+def smallest(numList):#TO FIND SMALLEST NUMBER
+    n=len(numList)
+    smallest_number=numList[0]#TAKING DUMMY VALUE
+    i=1
+    while (i<n):
+        if numList[i]<smallest_number:
+            smallest_number=numList[i]
+            i+=1
+    return smallest_number
+
+def my_sort(list):#TO SORT IT IN ASCENDING ORDER
+    k=len(list)
+    sort_list=[]
+    x=1
+    while (x<=k):
+        print(*list) # in-built function
+        sm = smallest(list) # user function
+        sort_list.append(sm)
+        list.remove(sm)
+        x+=1
+    print(*sort_list)
+
+my_sort([6,5,4,3,2,1])
